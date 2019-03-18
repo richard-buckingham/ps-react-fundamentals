cfdb33af5ec03a015b2357eabd0d48e53a277c58
richie.buckingham <richie.buckingham@gmail.com>
2019-03-18 13:34:25 +1100

    minor refactoring

---
+++ authorquiz/src/AuthorQuiz.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+class AuthorQuiz extends Component {
+  render() {
+    return <div>Author Quiz</div>;
+  }
+}
+
+export default AuthorQuiz;
