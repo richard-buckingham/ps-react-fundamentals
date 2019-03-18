362af7b766920c8dbfea9d7c27f29dc37a48ea10
richie.buckingham <richie.buckingham@gmail.com>
2019-03-18 12:00:25 +1100

    our first simple react app

--- click-counter/src/index.js
+++ click-counter/src/index.js
@@ -1,10 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+let model = { clicks: 10 };
+
+function render() {
+  ReactDOM.render(
+    <App
+      clicks={model.clicks}
+      onClick={() => {
+        console.log("in the onClick function");
+        model.clicks += 1;
+        // render the app after the click event occurs.
+        render();
+      }}
+    />,
+    document.getElementById("root")
+  );
+}
+
+// render the ui when the app starts
+render();
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
