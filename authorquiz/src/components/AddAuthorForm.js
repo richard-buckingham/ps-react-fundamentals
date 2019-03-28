9e2531025e4a98c88d1cb9a65cd00448c1c91adb
richie.buckingham <richie.buckingham@gmail.com>
2019-03-28 17:11:18 +1100

    add styilng to author form

--- authorquiz/src/components/AddAuthorForm.js
+++ authorquiz/src/components/AddAuthorForm.js
@@ -1,10 +1,23 @@
 import React from "react";
 
+import "./AddAuthorForm.css";
+
 function AddAuthorForm({ match }) {
   return (
-    <div>
+    <div className="AddAuthorForm">
+      {/* <p>{JSON.stringify(match)}</p> */}
       <h1>Add Author</h1>
-      <p>{JSON.stringify(match)}</p>
+      <form>
+        <div className="AddAuthorForm__input">
+          <label htmlFor="name">Name</label>
+          <input type="text" name="name" />
+        </div>
+
+        <div className="AddAuthorForm__input">
+          <label htmlFor="imageUrl">Image Url</label>
+          <input type="text" name="imageUrl" />
+        </div>
+      </form>
     </div>
   );
 }
